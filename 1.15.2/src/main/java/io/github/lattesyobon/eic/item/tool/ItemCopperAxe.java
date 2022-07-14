8d90b7bab37f2114004bdc3652fadbef4d187df4
latteS <90080815+LatteSyobon@users.noreply.github.com>
2022-07-15 06:30:36 +0900

    Fix

--- 1.15.2/src/main/java/io/github/lattesyobon/eic/item/tool/ItemCopperAxe.java
+++ 1.15.2/src/main/java/io/github/lattesyobon/eic/item/tool/ItemCopperAxe.java
@@ -1,6 +1,10 @@
 package io.github.lattesyobon.eic.item.tool;
 
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.IItemTier;
 
 public class ItemCopperAxe extends AxeItem {
+    public ItemCopperAxe(IItemTier p_i48530_1_, float p_i48530_2_, float p_i48530_3_, Properties p_i48530_4_) {
+        super(p_i48530_1_, p_i48530_2_, p_i48530_3_, p_i48530_4_);
+    }
 }
--- 1.15.2/src/main/java/io/github/lattesyobon/eic/item/tool/ItemCopperHoe.java
+++ 1.15.2/src/main/java/io/github/lattesyobon/eic/item/tool/ItemCopperHoe.java
@@ -1,6 +1,10 @@
 package io.github.lattesyobon.eic.item.tool;
 
 import net.minecraft.item.HoeItem;
+import net.minecraft.item.IItemTier;
 
 public class ItemCopperHoe extends HoeItem {
+    public ItemCopperHoe(IItemTier p_i48488_1_, float p_i48488_2_, Properties p_i48488_3_) {
+        super(p_i48488_1_, p_i48488_2_, p_i48488_3_);
+    }
 }
