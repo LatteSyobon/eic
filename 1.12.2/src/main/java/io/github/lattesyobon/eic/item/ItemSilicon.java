642e4aba4691b21eada0940440b593eff92c1dba
latteS <90080815+LatteSyobon@users.noreply.github.com>
2022-07-21 22:38:50 +0900

    Added Advancements

---
+++ 1.12.2/src/main/java/io/github/lattesyobon/eic/item/ItemSilicon.java
@@ -0,0 +1,60 @@
+
+package io.github.lattesyobon.eic.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import io.github.lattesyobon.eic.creativetab.TabEic;
+import io.github.lattesyobon.eic.ElementsEic;
+
+@ElementsEic.ModElement.Tag
+public class ItemSilicon extends ElementsEic.ModElement {
+	@GameRegistry.ObjectHolder("eic:silicon")
+	public static final Item block = null;
+	public ItemSilicon(ElementsEic instance) {
+		super(instance, 24);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("eic:silicon", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("silicon");
+			setRegistryName("silicon");
+			setCreativeTab(TabEic.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
