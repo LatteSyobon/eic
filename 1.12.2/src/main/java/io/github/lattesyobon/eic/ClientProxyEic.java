72223dd3816660a04ec30293f29ff36469fd5e32
latteS <90080815+LatteSyobon@users.noreply.github.com>
2022-07-21 20:02:21 +0900

    Enabled to develop with MCreator in minecraft 1.12.2.

---
+++ 1.12.2/src/main/java/io/github/lattesyobon/eic/ClientProxyEic.java
@@ -0,0 +1,26 @@
+package io.github.lattesyobon.eic;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyEic implements IProxyEic {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("eic");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
